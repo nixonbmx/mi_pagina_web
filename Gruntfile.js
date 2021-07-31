c0a8817df8e52bd819b29cec4edd0013d86eb901
nixon piñeres <nixonarmando8@gmail.com>
2021-07-31 15:29:56 -0500

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function (grunt){
+    grunt.initConfig({
+
+    });
+    grunt.registerTask('run',function(){
+        console.log('i am running');
+    });
+    grunt.registerTask('sleep',function(){
+        console.log('i am sleeping');
+    });
+   
+};
